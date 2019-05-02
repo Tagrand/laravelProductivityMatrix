a17f58de318d51ef8730e8ab6afe725e2542d2d3
Tom <24781073+Tagrand@users.noreply.github.com>
2019-05-02 20:45:49 +0100

    ✨  add matrix

---
+++ resources/js/components/TimeMatrix.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-64 w-64 flex flex-wrap">
+    <div class="bg-teal-brand h-32 w-32"></div>
+    <div class="bg-green-brand h-32 w-32"></div>
+    <div class="bg-red-brand h-32 w-32"></div>
+    <div class="bg-yellow-brand h-32 w-32"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {};
+    },
+  };
+</script>
