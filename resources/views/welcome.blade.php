d4d0c60bbb87fade80fcd992417fae8981c2afc3
Tom <24781073+Tagrand@users.noreply.github.com>
2019-05-02 19:27:54 +0100

    🏗  add vue to blade file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,20 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-        <link rel="stylesheet" type="text/css" href="/css/app.css">
-    </head>
-    <body>
-      <p class="text-center bg-blue">Another</p>
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Laravel</title>
+  <link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+
+<body>
+  <p class="text-center bg-blue" id="app">
+    <example-component></example-component>
+  </p>
+</body>
+
 </html>
+
+<script src="js/app.js"></script>
