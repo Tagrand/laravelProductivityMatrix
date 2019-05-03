cfc0fe815aac7a0127b541dd70ea7e185483a963
Tom <24781073+Tagrand@users.noreply.github.com>
2019-05-03 18:39:07 +0100

    🚧  add dummy data in

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -1,10 +1,38 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  state: {
-    change: true,
-  }
+    state: {
+        change: true,
+
+        jobs: [
+            {
+                name: "build website",
+                y: "765",
+                x: "265"
+            },
+            {
+                name: "build app",
+                y: "652",
+                x: "4"
+            },
+            {
+                name: "clean flat",
+                y: "8",
+                x: "800"
+            },
+            {
+                name: "phone home",
+                y: "989",
+                x: "877"
+            },
+            {
+                name: "watch netflix",
+                y: "2",
+                x: "76"
+            }
+        ]
+    }
 });
