dc08812c18233531c82af1c0b4bc5c785f41573c
Tom <24781073+Tagrand@users.noreply.github.com>
2019-05-06 09:05:08 +0100

    🗃  no seeding on production

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(JobsSeeder::class);
+        if (app()->environment() !== 'production') {
+            $this->call(JobsSeeder::class);
+        }
     }
 }
